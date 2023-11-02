969dfcb3c0dbf1bb22f2cfd8973d9f8770fd182c
Nikolay-Kovalov <lawyerpartners.ua@gmail.com>
2023-11-02 21:32:59 +0200

    commit

--- mob-menu.js
+++ mob-menu.js
@@ -3,9 +3,12 @@
 const mobMenuBtn = document.querySelector(".mob-menu-btn");
 const mobMenu = document.querySelector(".mobile_menu");
 const backdrop = document.querySelector(".backdrop");
+const navList = document.querySelector(".navigation_list")
+console.log(navList)
 
 backdrop.addEventListener('click', onBackdropClickMob);
 mobMenuBtn.addEventListener('click', onMobMenuBtnClick);
+navList.addEventListener('click', onMobItemClick)
 
 function onMobMenuBtnClick() {
        backdrop.classList.remove('hidden');
@@ -20,4 +23,13 @@ function onBackdropClickMob(evt) {
         mobMenu.classList.add('hidden-mob');
         document.body.classList.remove('backdrop_active')
     } 
+}
+function onMobItemClick(evt) {
+    if (evt.target.classList.contains('mob')) {
+          backdrop.classList.add('hidden');
+        mobMenu.classList.add('hidden-mob');
+        document.body.classList.remove('backdrop_active') 
+        console.log("jnbjh")
+    }
+
 }
