cc63d79ba482559f5a62262103896e638d68e207
Nikolay-Kovalov <lawyerpartners.ua@gmail.com>
2023-11-02 23:26:28 +0200

    commit

--- mob-menu.js
+++ mob-menu.js
@@ -2,7 +2,7 @@
 
 const mobMenuBtn = document.querySelector(".mob-menu-btn");
 const mobMenu = document.querySelector(".mobile_menu");
-const backdrop = document.querySelector(".backdrop");
+const backdrop = document.querySelector(".backdrop-mob");
 const navList = document.querySelector(".navigation_list")
 console.log(navList)
 
@@ -11,7 +11,7 @@ mobMenuBtn.addEventListener('click', onMobMenuBtnClick);
 navList.addEventListener('click', onMobItemClick)
 
 function onMobMenuBtnClick() {
-       backdrop.classList.remove('hidden');
+       backdrop.classList.remove('hidden-mob');
         mobMenu.classList.remove('hidden-mob');
     document.body.classList.add('backdrop_active')   
    
@@ -19,17 +19,16 @@ function onMobMenuBtnClick() {
 
 function onBackdropClickMob(evt) {
       if (evt.target === evt.currentTarget) {
-        backdrop.classList.add('hidden');
+        backdrop.classList.add('hidden-mob');
         mobMenu.classList.add('hidden-mob');
         document.body.classList.remove('backdrop_active')
     } 
 }
 function onMobItemClick(evt) {
     if (evt.target.classList.contains('mob')) {
-          backdrop.classList.add('hidden');
+          backdrop.classList.add('hidden-mob');
         mobMenu.classList.add('hidden-mob');
         document.body.classList.remove('backdrop_active') 
-        console.log("jnbjh")
     }
 
 }
